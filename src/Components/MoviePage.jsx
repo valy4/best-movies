5a367eff4fa22da9918603d6ffef6a8bb2581f67
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-07-26 20:22:20 +0100

    favorite button added and stored in local storage

--- src/Components/MoviePage.jsx
+++ src/Components/MoviePage.jsx
@@ -3,57 +3,86 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Image from "../assets/images/sorry-image-not-available.png"
+import Image from "../assets/images/sorry-image-not-available.png";
 
 function MoviePage() {
   let { imdbID } = useParams();
-  const [details, setDetails] = useState([]);
+  const [favorites, setFavorites] = useState(
+    localStorage.getItem("favorites") ? localStorage.getItem("favorites").split(",") : []
+  );
+  const [details, setDetails] = useState({});
 
   useEffect(() => {
     fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=20b3b01a`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setDetails((details) => [...details, data]);
+        setDetails(data);
       });
   }, [imdbID]);
 
+  useEffect(() => {
+    localStorage.setItem("favorites", favorites);
+  }, [favorites]);
+
+  console.log(favorites);
   return (
     <div>
       <div>
-        {details.map((detail) => (
-          <ContainerPage>
-            {detail.Poster !== "N/A" && (
+        <ContainerPage>
+          {details.Poster !== "N/A" && <Poster src={details.Poster}></Poster>}
+          {details.Poster === "N/A" && (
+            <Poster src={Image} alt={details.Title}></Poster>
+          )}
 
-              <Poster src={detail.Poster}></Poster>
-
-            )
-            }
-            {
-              detail.Poster === "N/A" && (
-                <Poster
-                  src={Image}
-                  alt={detail.Title}
-                ></Poster>
-              )
-            }
-
-            <Text>
-              <Title>{detail.Title}</Title>
-              {detail.imdbRating !== "N/A" && (
-                <p>
-                  Rating <b style={{ color: "#fca311" }}>{detail.imdbRating}</b>
-                </p>)}
-              {detail.imdbRating === "N/A" && (<p>
+          <Text>
+            <Title>{details.Title}</Title>
+            {details.imdbRating !== "N/A" && (
+              <p>
+                Rating <b style={{ color: "#fca311" }}>{details.imdbRating}</b>
+              </p>
+            )}
+            {details.imdbRating === "N/A" && (
+              <p>
                 Rating <b style={{ color: "#fca311" }}>Not Available</b>
-              </p>)}
-              <button className="btn1">Add to favorite</button>
-              {detail.Plot !== "N/A" && (<Plot>{detail.Plot}</Plot>)
-              }
-              {detail.Plot === "N/A" && <Plot>Sorry no description available!</Plot>}
-            </Text>
-          </ContainerPage>
-        ))}
+              </p>
+            )}
+            {favorites.includes(details.Title) ? (
+              <button
+                disabled={!favorites.includes(details.Title)}
+                className="btn1"
+                onClick={() => {
+                  // filter the ones that are not our favourite
+                  const newFavList = favorites.filter(
+                    (fav) => fav !== details.Title
+                  );
+
+                  // update state
+                  setFavorites(newFavList);
+                }}
+              >
+                Remove from favorites
+              </button>
+            ) : (
+              <button
+                disabled={favorites.includes(details.Title)}
+                className="btn1"
+                onClick={() => {
+                  if (!favorites.includes(details.Title)) {
+                    setFavorites([...favorites, details.Title]);
+                  }
+                }}
+              >
+                Add to favorite
+              </button>
+            )}
+
+            {details.Plot !== "N/A" && <Plot>{details.Plot}</Plot>}
+            {details.Plot === "N/A" && (
+              <Plot>Sorry no description available!</Plot>
+            )}
+          </Text>
+        </ContainerPage>
       </div>
 
       <Link to={`/`}>
